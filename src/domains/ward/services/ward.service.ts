ebd011d99418f11ceedbaa214bd86f8de97e31d3
indraphand-mb <indra.phand@mindbowser.com>
2026-05-06 16:55:32 +0530

    feat-ward summary code added.

--- src/domains/ward/services/ward.service.ts
+++ src/domains/ward/services/ward.service.ts
@@ -2,6 +2,8 @@ import { Service } from "typedi";
 import { AppDataSource } from "../../../db/db";
 import { Ward } from "../entity/ward.entity";
 import { Bed } from "../../bed/entity/bed.entity";
+import { log } from "node:console";
+import { success } from "../../../Http_Response/response";
 
 @Service()
 export class WardService {
@@ -9,11 +11,43 @@ export class WardService {
   private bedRepo = AppDataSource.getRepository(Bed);
 
   public async getAllWards() {
-    const wards = await this.wardRepo.find({
-      relations: ["beds", "users"],
-    });
+    const wards = await this.wardRepo.find();
 
     return wards;
+  }
+
+  public async getWardSummary() {
+    const wards = await this.wardRepo.find({
+      relations: ["beds"],
+    });
+
+    // console.log(wards);
+
+    const data = wards.map((ward) => {
+      const availableBeds = ward.beds.filter(
+        (bed) => bed.status === "AVAILABLE",
+      ).length;
+      const occupiedBeds = ward.beds.filter(
+        (bed) => bed.status === "OCCUPIED",
+      ).length;
+      const cleaningBeds = ward.beds.filter(
+        (bed) => bed.status === "CLEANING",
+      ).length;
+
+      return {
+        id: ward.id,
+        name: ward.name,
+        type: ward.type,
+        capacity: ward.capacity,
+        description: ward.description,
+
+        availableBeds,
+        occupiedBeds,
+        cleaningBeds,
+      };
+    });
+
+    return data;
   }
 
   public async createWard(
@@ -22,9 +56,16 @@ export class WardService {
     capacity: number,
     description?: string,
   ) {
-    const existing = await this.wardRepo.findOne({ where: { name } });
+    const existing = await this.wardRepo.findOne({
+      where: { name },
+    });
+
     if (existing) {
-      throw new Error("Ward already exists");
+      return {
+        success: false,
+        message: "Ward already exists",
+        data: null,
+      };
     }
 
     const ward = this.wardRepo.create({
@@ -37,16 +78,22 @@ export class WardService {
     await this.wardRepo.save(ward);
 
     const beds: Bed[] = [];
+
     for (let i = 1; i <= capacity; i++) {
       const bed = this.bedRepo.create({
         ward,
         bed_number: `${name}-${i}`,
       });
+
       beds.push(bed);
     }
 
     await this.bedRepo.save(beds);
 
-    return ward;
+    return {
+      success: true,
+      message: "Ward created successfully",
+      data: ward,
+    };
   }
 }
